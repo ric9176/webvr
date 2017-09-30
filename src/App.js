a54ddedc3b31d0cee495996a8d1430cb60be3421
Richard Moss <richard@englishtutorslive.com>
2017-09-30 18:08:04 +0100

    Adds random shapes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import 'aframe'
 
 class App extends Component {
   render() {
     return (
-      
+      <a-scene>
+      <a-entity
+        geometry="primitive: box"
+        position="-1 0.5 -3"
+        rotation="0 45 0"
+        material="color: #4CC3D9" />
+      <a-entity
+        geometry="primitive: sphere; radius: 1.25;"
+        position="0.080 3.414 -3.983"
+        material="color: #EF2D5E" />
+      <a-entity
+        geometry="primitive: cylinder; radius: 0.5, height: 1.5"
+        position="1 0.75 -3"
+        material="color: #FFC65D" />
+      <a-entity
+        geometry="primitive: plane; width: 4; height: 4"
+        position="0 0 -4"
+        rotation="-90 0 0"
+        material="color: #7BC8A4" />
+      <a-entity
+        geometry="primitive: sphere; radius: 1.25;"
+        position="1.492 3.497 3.072"
+        material="color: #ffff00" />
+      </a-scene>
     )
   }
 }
 
-export default App;
+export default App
